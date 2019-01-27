15111fb2394e57000ffafef5a72f2a9d6cd81c7b
Brendan Lau <Brendan>
2019-01-27 00:59:27 -0800

    added button and center

--- RandomNumbWindow.py
+++ RandomNumbWindow.py
@@ -1,13 +1,41 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import (QDesktopWidget, QWidget, QToolTip, QPushButton, QApplication)
+from PyQt5.QtGui import QIcon, QFont
+# import RandomNumb.py
+
+
+class RandomNumb(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        QToolTip.setFont(QFont('TimesNewRoman', 10))
+
+        # creates a button that the user will use to submit guess
+        submit = QPushButton('Submit', self)
+        submit.setToolTip('This will submit your guess')
+        submit.resize(submit.sizeHint())
+        submit.move(350, 500)
+
+        # creates the window properties
+        self.resize(700, 700)
+        self.center()
+
+        self.setWindowTitle('Random Number Guesser')
+        self.show()
+
+    # centers the window in the middle of the screen
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
 
 # create a movable, resizable window
 if __name__ == '__main__':
+    # creates application object with command line args
     app = QApplication(sys.argv)
-    w = QWidget()
-    w.resize(750, 750)
-    w.move(300, 300)
-    w.setWindowTitle('Random Number Guesser')
-    w.show()
-
+    rn = RandomNumb()
     sys.exit(app.exec_())
